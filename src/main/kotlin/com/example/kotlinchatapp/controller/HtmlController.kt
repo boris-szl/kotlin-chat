3d07de7b1bd038fffaca2cc98bb8d10bec4e74c4
boris-szl <boris.szelcsanyi@outlook.com>
2023-07-08 16:49:21 +0200

    feat: define html controller for exposing html page via thymeleaf

---
+++ src/main/kotlin/com/example/kotlinchatapp/controller/HtmlController.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinchatapp.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+
+class HtmlController(val messageService: MessageService) {
+
+    @GetMapping("/")
+    fun index(model: Model): String {
+        val messages = messageService.latest()
+
+        model["messages"] = messages
+        model["lastMessageId"] = messages.lastOrNull()?.id ?: "" // safe call operator, elvis operator
+        return "chat"
+
+    }
+}
