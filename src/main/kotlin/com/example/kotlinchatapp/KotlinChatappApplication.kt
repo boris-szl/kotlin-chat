25945ce4e4a883c66f7ccd82aa2ec8f345d51cdb
boris-szl <boris.szelcsanyi@outlook.com>
2023-07-08 12:00:51 +0200

    feat: init project

---
+++ src/main/kotlin/com/example/kotlinchatapp/KotlinChatappApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinchatapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinChatappApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinChatappApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinchatapp/KotlinChatappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinchatapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinChatappApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
