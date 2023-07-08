0b7f11f7084bd0aa48697c88b0954da08f8a4b99
boris-szl <boris.szelcsanyi@outlook.com>
2023-07-08 19:07:24 +0200

    feat: add render function for markdown contentype

--- src/main/kotlin/com/example/kotlinchatapp/extension/Extensions.kt
+++ src/main/kotlin/com/example/kotlinchatapp/extension/Extensions.kt
@@ -1,12 +1,15 @@
 package com.example.kotlinchatapp.extension
 
-import com.example.kotlinchatapp.domainmodel.ContentType
 import com.example.kotlinchatapp.domainmodel.Message
+import com.example.kotlinchatapp.enum.ContentType
 import com.example.kotlinchatapp.viewmodel.MessageVM
 import com.example.kotlinchatapp.viewmodel.UserVM
+import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
+import org.intellij.markdown.html.HtmlGenerator
+import org.intellij.markdown.parser.MarkdownParser
 import java.net.URL
 
-fun MessageVM.asDomainObject(contentType: ContentType = ContentType.PLAIN): Message = Message(
+fun MessageVM.asDomainObject(contentType: ContentType = ContentType.MARKDOWN): Message = Message(
         content,
         contentType,
         sent,
@@ -16,7 +19,7 @@ fun MessageVM.asDomainObject(contentType: ContentType = ContentType.PLAIN): Mess
 )
 
 fun Message.asViewModel(): MessageVM = MessageVM(
-        content,
+        contentType.render(content),
         UserVM(username, URL(userAvatarImageLink)),
         sent,
         id
@@ -26,4 +29,9 @@ fun List<Message>.mapToViewModel(): List<MessageVM> = map { it.asViewModel() }
 
 fun ContentType.render(content: String): String = when (this) {
     ContentType.PLAIN -> content
+    ContentType.MARKDOWN -> {
+        val flavour = CommonMarkFlavourDescriptor()
+        HtmlGenerator(content, MarkdownParser(flavour).buildMarkdownTreeFromString(content),
+                flavour).generateHtml()
+    }
 }
